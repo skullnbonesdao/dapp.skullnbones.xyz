27b88e8be283f41192e4470ff13b133c8d959a40
skullnbonesdao <skullnbonesdao@users.noreply.github.com>
2023-11-12 14:34:55 +0000

    deploy: fbb2650711918805166635fb56588fea46fdec32

---
+++ assets/AccountsPage.a2bbd54f.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.09b4ef2f.js";import{_ as r}from"./plugin-vue_export-helper.21dcd24c.js";import{b1 as t,ai as a,bx as c,ar as o}from"./index.e8b36f48.js";const s={};function n(f,_){return t(),a(e,{class:"row items-center justify-evenly"},{default:c(()=>[o(" BURN")]),_:1})}var p=r(s,[["render",n]]);export{p as default};
