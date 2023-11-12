c5d992dcae3c3cf1205192aac45254aadde00feb
Skullnbones <skullandbonesdao@gmail.com>
2023-11-12 19:01:49 +0100

    added accounts page

--- src/components/Accounts/AccountTemplate.vue
+++ src/components/Accounts/AccountTemplate.vue
@@ -4,40 +4,99 @@ import * as factory from '@staratlas/factory';
 import { PublicKey } from '@solana/web3.js';
 import { useWallet } from 'solana-wallets-vue';
 import { biAward } from '@quasar/extras/bootstrap-icons';
+import { useGlobalStore } from 'stores/globalStore';
+import * as spl from '@solana/spl-token';
+import { getAssociatedTokenAddress, TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import { ASSOCIATED_PROGRAM_ID } from '@coral-xyz/anchor/dist/cjs/utils/token';
 
-const props = defineProps(['count', 'function_name', 'publicKey']);
+const props = defineProps([
+  'count',
+  'function_name',
+  'publicKey',
+  'mint',
+  'program',
+]);
 
 const data = ref();
 
 const SYSTEM_PROGRAM = '11111111111111111111111111111111';
 
 const ATLAS_LOCKER_PROGRAM = 'ATLocKpzDbTokxgvnLew3d7drZkEzLzDpzwgrgWKDbmc';
+const ATLAS_LOCKER_PROGRAM_AUTH =
+  'ATLkZsBofSKG845dNFpNoUyMciGpeH29BCbMqYFUoxzU';
+
 const POLIS_MINT = 'poLisWXnNRwC6oBu1vHiuKQzFjGL4XDSu4g9qjz9qVk';
 const ATLAS_MINT = 'ATLASXmbPQxBUYbxPsV97usA3fPQYEqzQBUHgiFCUsXx';
 
 async function run_querry() {
   switch (props.function_name) {
+    case 'getRegisteredStake':
+      {
+        //J5GhV1WKcEU98c1kZt36ixjaErrrPNWbZhg3JQDg114E
+        const [registeredStake] = await factory.getRegisteredStake(
+          new PublicKey(ATLAS_LOCKER_PROGRAM),
+          new PublicKey(ATLAS_LOCKER_PROGRAM_AUTH),
+          new PublicKey(props.mint),
+          new PublicKey(POLIS_MINT),
+        );
+        data.value = registeredStake;
+      }
+      break;
+
     case 'getStakingAccount':
       {
+        const [registeredStake] = await factory.getRegisteredStake(
+          new PublicKey(ATLAS_LOCKER_PROGRAM),
+          new PublicKey(ATLAS_LOCKER_PROGRAM_AUTH),
+          new PublicKey(props.mint),
+          new PublicKey(POLIS_MINT),
+        );
+
         //7AUgG8ghecQYopPqVVnjL4x79VeA93TQRLFcQq4Mqehd
-        data.value = await factory.getStakingAccount(
+        const [statking_account] = await factory.getStakingAccount(
           new PublicKey(ATLAS_LOCKER_PROGRAM),
           new PublicKey(props.publicKey),
-          new PublicKey('J5GhV1WKcEU98c1kZt36ixjaErrrPNWbZhg3JQDg114E'),
+          registeredStake,
+        );
+        data.value = statking_account;
+      }
+      break;
+
+    case 'getTokenEscrow':
+      {
+        const [registeredStake] = await factory.getRegisteredStake(
+          new PublicKey(ATLAS_LOCKER_PROGRAM),
+          new PublicKey(ATLAS_LOCKER_PROGRAM_AUTH),
+          new PublicKey(props.mint),
+          new PublicKey(POLIS_MINT),
+        );
+
+        const [statking_account] = await factory.getStakingAccount(
+          new PublicKey(ATLAS_LOCKER_PROGRAM),
+          new PublicKey(props.publicKey),
+          registeredStake,
+        );
+
+        //FbkSCpg5WoPMyQfRfQZWudwHuuts94RcPGPdaTAwyjy1
+        data.value = await getAssociatedTokenAddress(
+          new PublicKey(props.mint),
+          statking_account,
+          true,
         );
       }
       break;
 
-    case 'getRegisteredStake':
+    case 'getAssociatedTokenAddress(mint)':
       {
-        //J5GhV1WKcEU98c1kZt36ixjaErrrPNWbZhg3JQDg114E
-        data.value = await factory.getRegisteredStake(
-          new PublicKey(ATLAS_LOCKER_PROGRAM),
-          new PublicKey(ATLAS_LOCKER_PROGRAM),
-          new PublicKey(ATLAS_MINT),
-          new PublicKey(ATLAS_MINT),
+        data.value = await getAssociatedTokenAddress(
+          new PublicKey(props.mint),
+          new PublicKey(props.publicKey),
+          true,
+          TOKEN_PROGRAM_ID,
+          ASSOCIATED_PROGRAM_ID,
         );
       }
+
       break;
     default:
       data.value = 'No function called';
--- src/pages/AccountsPage.vue
+++ src/pages/AccountsPage.vue
@@ -4,11 +4,13 @@ import { useWallet } from 'solana-wallets-vue';
 import { onMounted, ref } from 'vue';
 import AccountTemplate from 'components/Accounts/AccountTemplate.vue';
 import AccountStatic from 'components/Accounts/AccountStatic.vue';
+import { matInbox } from '@quasar/extras/material-icons';
 
 const count = ref(0);
 const data = ref();
 
 const public_key = ref('none');
+const token_mint = ref('ATLASXmbPQxBUYbxPsV97usA3fPQYEqzQBUHgiFCUsXx');
 
 onMounted(() => {
   public_key.value = useWallet().publicKey.value?.toString() ?? 'none';
@@ -21,29 +23,46 @@ const send = () => {
 
 <template>
   <q-page class="col items-center justify-evenly q-mx-sm q-pt-sm q-gutter-y-md">
-    <div class="row">
-      <q-input
-        class="col"
-        v-model="public_key"
-        label="Wallet Address"
-      ></q-input>
+    <div class="row q-gutter-sm">
+      <div class="col">
+        <q-input
+          class="col"
+          v-model="public_key"
+          label="Wallet Address"
+        ></q-input>
+        <q-input class="col" v-model="token_mint" label="Token Mint"></q-input>
+      </div>
       <q-btn color="primary" icon="send" @click="send" />
     </div>
 
-    <AccountStatic
-      function_name="getRegisteredStake(Atlas)"
-      data="J5GhV1WKcEU98c1kZt36ixjaErrrPNWbZhg3JQDg114E"
-    ></AccountStatic>
+    <div>Solana</div>
+    <AccountTemplate
+      :count="count"
+      function_name="getAssociatedTokenAddress(mint)"
+      :public-key="public_key"
+      :mint="token_mint"
+    />
+
+    <div>StarAtlas-Locker</div>
 
     <AccountTemplate
       :count="count"
       function_name="getRegisteredStake"
       :public-key="public_key"
+      :mint="token_mint"
     />
     <AccountTemplate
       :count="count"
       function_name="getStakingAccount"
       :public-key="public_key"
+      :mint="token_mint"
+    />
+
+    <AccountTemplate
+      :count="count"
+      function_name="getTokenEscrow"
+      :public-key="public_key"
+      :mint="token_mint"
     />
   </q-page>
 </template>
