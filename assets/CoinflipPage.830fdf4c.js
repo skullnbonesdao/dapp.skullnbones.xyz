dd886d18ea0fb4f6cde22515fbff2678a1624ff5
skullnbonesdao <skullnbonesdao@users.noreply.github.com>
2023-11-16 02:27:19 +0000

    deploy: a092cd49004460852b467760c8b8491085fee012

---
+++ assets/CoinflipPage.830fdf4c.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.b59e38ab.js";import{_ as r}from"./plugin-vue_export-helper.21dcd24c.js";import{b1 as a,ai as t,bx as o,ar as s}from"./index.95f0c95e.js";const c={};function f(i,n){return a(),t(e,{class:"row items-center justify-evenly"},{default:o(()=>[s(" COINFLIP")]),_:1})}var p=r(c,[["render",f]]);export{p as default};
