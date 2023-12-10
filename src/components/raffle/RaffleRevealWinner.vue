1b27d27a03c21ae4708cf257c3385087f6b2d1ca
SnB <skullandbonesdao@gmail.com>
2023-12-10 19:26:24 +0100

    added drawing animation

--- src/components/raffle/RaffleRevealWinner.vue
+++ src/components/raffle/RaffleRevealWinner.vue
@@ -5,10 +5,14 @@ import { SYSVAR_RECENT_BLOCKHASHES_PUBKEY } from '@solana/web3.js';
 import { Notify } from 'quasar';
 import { useWorkspaceAdapter } from 'src/idls/adapter/apapter';
 import { handle_confirmation } from 'components/messages/handle_confirmation';
+import BurningTicket from 'components/video/BurningTicket.vue';
+import { useRaffleStore } from 'stores/globalRaffle';
+import { useGlobalWalletStore } from 'stores/globalWallet';
 
 const input_raffle_ticket_amount = ref();
 
 const props = defineProps(['raffle', 'is_admin']);
+const show_drawing_animation = ref(false);
 
 async function reveal_raffle_winner() {
   const { pg_raffle, pg_whitelist } = useWorkspaceAdapter();
@@ -30,9 +34,13 @@ async function reveal_raffle_winner() {
       })
       .rpc();
 
+    show_drawing_animation.value = true;
     console.log(signature);
 
     await handle_confirmation(signature);
+
+    await useRaffleStore().update_raffles();
+    show_drawing_animation.value = false;
   } catch (err) {
     Notify.create({
       color: 'red',
@@ -57,6 +65,24 @@ async function reveal_raffle_winner() {
       label="Reveal Winner"
       @click="reveal_raffle_winner()"
     />
+
+    <q-dialog :maximized="true" v-model="show_drawing_animation">
+      <q-card flat square class="">
+        <q-card-section>
+          <div class="text-h6 text-center">Drawing...</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none row">
+          <q-space />
+          <BurningTicket />
+          <q-space />
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="close" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
 
