4436113c479fac6addfd20cee4df8e73821f5486
skullnbonesdao <skullnbonesdao@users.noreply.github.com>
2023-12-18 19:16:19 +0000

    deploy: e98a4ebf1e34984d2a3a35ea41c79b7f690ad9b1

---
+++ assets/QCardActions.c0005919.js
@@ -0,0 +1,1 @@
+import{bZ as t,dT as r,dU as c,ah as n,aF as l,cg as i}from"./index.60f956d2.js";var d=t({name:"QCardActions",props:{...r,vertical:Boolean},setup(a,{slots:s}){const e=c(a),o=n(()=>`q-card__actions ${e.value} q-card__actions--${a.vertical===!0?"vert column":"horiz row"}`);return()=>l("div",{class:o.value},i(s.default))}});export{d as Q};
---
+++ assets/WrapperPage.8c7d23b2.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.f44b669e.js";import{_ as r}from"./plugin-vue_export-helper.21dcd24c.js";import{b1 as a,ai as t,bx as o,ar as s}from"./index.60f956d2.js";const c={};function f(n,_){return a(),t(e,{class:"row items-center justify-evenly"},{default:o(()=>[s(" WRAPPER")]),_:1})}var l=r(c,[["render",f]]);export{l as default};
