7a08a625b1930a59bc4a02ba06225167352b3a1a
skullnbonesdao <skullnbonesdao@users.noreply.github.com>
2024-09-01 00:35:48 +0000

    deploy: dc2b6a54b168d9895dfc37a9fdb2901e80b0313c

---
+++ assets/BurnPage.7cf7c6d5.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.57cdb4a4.js";import{_ as r}from"./plugin-vue_export-helper.21dcd24c.js";import{b1 as a,ai as t,bx as o,ar as s}from"./index.a7087e37.js";const c={};function n(f,_){return a(),t(e,{class:"row items-center justify-evenly"},{default:o(()=>[s(" BURN")]),_:1})}var p=r(c,[["render",n]]);export{p as default};
---
+++ assets/QSpace.93fafbd1.js
@@ -0,0 +1,1 @@
+import{aF as a,bZ as e}from"./index.a7087e37.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
