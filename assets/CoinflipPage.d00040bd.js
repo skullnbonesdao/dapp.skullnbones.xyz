198fcbd15b53e9556949d49d440bab55b10b8bc2
skullnbonesdao <skullnbonesdao@users.noreply.github.com>
2024-11-04 23:07:40 +0000

    deploy: 9ee1bfa70abc0a7a24dbe15daefbd98dc63acd79

--- assets/CoinflipPage.d00040bd.js
+++
@@ -1,1 +0,0 @@
-import{Q as e}from"./QPage.0ee2fc37.js";import{_ as r}from"./plugin-vue_export-helper.21dcd24c.js";import{b1 as a,ai as t,bx as o,ar as s}from"./index.2678ff24.js";const c={};function f(i,n){return a(),t(e,{class:"row items-center justify-evenly"},{default:o(()=>[s(" COINFLIP")]),_:1})}var p=r(c,[["render",f]]);export{p as default};
--- assets/axios.e83aa84f.js
+++
@@ -1,1 +0,0 @@
-import{b as i,a}from"./index.2678ff24.js";const s="https://api.jsonbin.io/v3/b/",e=a.create({baseURL:"https://api.example.com"}),t=a.create({baseURL:s});var r=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e,o.config.globalProperties.$apiJsonBin=t,console.log("[booted] axios")});export{e as api,t as apiJsonBin,r as default};
