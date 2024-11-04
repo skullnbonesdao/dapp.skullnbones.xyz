df6387294cfd27cb949225f2c49b33aed5de3bda
skullnbonesdao <skullnbonesdao@users.noreply.github.com>
2024-11-04 01:12:38 +0000

    deploy: 6b928745ba37d198287d3367543f7650344770c3

---
+++ assets/QCardActions.9ffdb5a1.js
@@ -0,0 +1,1 @@
+import{bZ as t,dX as r,dY as c,ah as n,aF as l,cg as i}from"./index.a4fb67a3.js";var d=t({name:"QCardActions",props:{...r,vertical:Boolean},setup(a,{slots:s}){const e=c(a),o=n(()=>`q-card__actions ${e.value} q-card__actions--${a.vertical===!0?"vert column":"horiz row"}`);return()=>l("div",{class:o.value},i(s.default))}});export{d as Q};
