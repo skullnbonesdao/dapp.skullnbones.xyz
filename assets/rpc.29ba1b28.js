4a331d89a544c4847a8472fb03d5005ea88b20e7
skullnbonesdao <skullnbonesdao@users.noreply.github.com>
2024-11-13 09:42:33 +0000

    deploy: 2ee24fe00d9c7983a68f6a93d37d0a00f1d9df98

---
+++ assets/rpc.29ba1b28.js
@@ -0,0 +1,1 @@
+/* empty css                       */import{b as o}from"./index.af9b7e12.js";var a=o(({app:p})=>{console.log("[booted] RPC")});export{a as default};
--- assets/rpc.5ba338d7.js
+++
@@ -1,1 +0,0 @@
-/* empty css                       */import{b as o}from"./index.9d536988.js";var a=o(({app:p})=>{console.log("[booted] RPC")});export{a as default};
