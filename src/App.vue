9f21ea74c1c9bdd83d071fb54d0250e94d96c1ef
skullandbonesdao <skullandbonesdao@gmail.com>
2024-11-14 11:43:36 +0100

    updated account store

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,12 @@ import {
   initWorkspaceAdapter,
   useWorkspaceAdapter,
 } from 'src/idls/adapter/apapter';
-import { onMounted } from 'vue';
+import { onMounted, watch } from 'vue';
 import 'src/css/backgrounds.scss';
 import { useRPCStore } from 'stores/rpcStore';
 import { useLockerPolisStore } from 'stores/globalLockerPolisStore';
+import { useWallet } from 'solana-wallets-vue';
+import { useAccountStore } from 'stores/globalAccountStore';
 
 useRPCStore();
 useRPCStore().update_connection();
@@ -31,7 +33,16 @@ useRaffleStore();
 
 useQuasar().dark.set(true);
 
+watch(
+  () => useWallet().publicKey.value,
+  async () => {
+    await useAccountStore().loadAccounts();
+  },
+);
+
 onMounted(async () => {
+  if (useWallet().publicKey.value) await useAccountStore().loadAccounts();
+
   await useGlobalStore().load_token_list();
   await useWhitelist().update_whitelist();
 });
