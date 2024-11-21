2bf35d5b885c5e6dd8d42a16937ddb7866cb8870
skullandbonesdao <skullandbonesdao@gmail.com>
2024-11-21 18:20:26 +0100

    added squads sign

--- src/solana/handleTransaction.ts
+++ src/solana/handleTransaction.ts
@@ -1,7 +1,14 @@
-import { Connection, Transaction } from '@solana/web3.js';
+import {
+  Connection,
+  PublicKey,
+  Transaction,
+  TransactionMessage,
+} from '@solana/web3.js';
 import { useWallet } from 'solana-wallets-vue';
 import { useRPCStore } from 'stores/rpcStore';
 import { Notify } from 'quasar';
+import { useSquadsStore } from 'src/solana/squads/SquadsStore';
+import * as multisig from '@sqds/multisig';
 
 export const handleTransaction = async (
   tx: Transaction,
@@ -23,10 +30,37 @@ export const handleTransaction = async (
       caption: 'Waiting for user to sign...',
     });
 
-    const signature = await sendTransaction(
-      tx,
-      useRPCStore().connection as Connection,
-    );
+    let signature = '';
+    if (!useSquadsStore().useSquads) {
+      signature = await sendTransaction(
+        tx,
+        useRPCStore().connection as Connection,
+      );
+    } else {
+      const blockhash = await useRPCStore().connection.getRecentBlockhash();
+      const transactionMessage = new TransactionMessage({
+        payerKey: new PublicKey(useSquadsStore().vaultPDA),
+        recentBlockhash: blockhash.blockhash,
+        instructions: tx.instructions,
+      });
+
+      await useSquadsStore().loadMultisigInfo();
+
+      const squadsTX = multisig.instructions.vaultTransactionCreate({
+        multisigPda: new PublicKey(useSquadsStore().multisigPDA),
+        transactionIndex: useSquadsStore().getNewTransactionIndex,
+        creator: useWallet().publicKey.value!,
+        vaultIndex: 0,
+        ephemeralSigners: 0,
+        transactionMessage: transactionMessage,
+        memo: label,
+      });
+
+      signature = await sendTransaction(
+        new Transaction().add(squadsTX),
+        useRPCStore().connection as Connection,
+      );
+    }
 
     notif({
       caption: `Waiting for confirmation: ${signature}`,
