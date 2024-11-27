9d0816df83f0a63826f768a02f4025aa3b86fbee
skullandbonesdao <skullandbonesdao@gmail.com>
2024-11-27 14:05:27 +0100

    added squad info to signing button

--- src/components/squads/SquadsButton.vue
+++ src/components/squads/SquadsButton.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import { useSquadsStore } from 'src/solana/squads/SquadsStore';
+import { format_address } from 'src/functions/format_address';
 </script>
 
 <template>
   <q-card square flat class="squads-buttons">
-    <q-toggle label="Sign as squads?" v-model="useSquadsStore().useSquads" />
+    <q-toggle
+      :label="
+        'Squad: ' + format_address(useSquadsStore().getVaultPDA.toString(), 3)
+      "
+      v-model="useSquadsStore().useSquads"
+      checked-icon="check"
+      unchecked-icon="clear"
+    />
   </q-card>
 </template>
 
