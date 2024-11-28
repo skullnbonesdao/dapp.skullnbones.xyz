a405cec657db4d26c7a8fb5babd7479461bc44f0
skullandbonesdao <skullandbonesdao@gmail.com>
2024-11-28 14:11:16 +0100

    added solscan account link buttons

---
+++ src/components/wrapper/WrapperSolscanLink.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps(['account']);
+</script>
+
+<template>
+  <q-btn
+    :href="`https://solscan.io/account/${props.account}`"
+    target="_blank"
+    color="primary"
+    dense
+  >
+    <q-avatar size="20px">
+      <img src="solscan.png" />
+    </q-avatar>
+  </q-btn>
+</template>
+
+<style scoped lang="sass"></style>
--- src/components/wrapper/WrapperTable.vue
+++ src/components/wrapper/WrapperTable.vue
@@ -2,7 +2,6 @@
 import { useQuasar } from 'quasar';
 import { computed, ref, watch } from 'vue';
 import { useWrapperStore } from 'src/solana/wrapper/WrapperStore';
-import { UNKNOWN_IMAGE } from 'stores/constants';
 import WrapperWrap from 'components/wrapper/WrapperWrap.vue';
 import WrapperUnwrap from 'components/wrapper/WrapperUnwrap.vue';
 import WrapperVaultDonut from 'components/wrapper/WrapperVaultDonut.vue';
@@ -10,7 +9,9 @@ import { useTokenListStore } from 'src/solana/tokens/TokenListStore';
 import { useAccountStore } from '../../solana/accounts/AccountStore';
 import FormatNumber from 'components/text/FormatNumber.vue';
 import { format_address } from '../../functions/format_address';
-import { getSigner } from 'src/solana/squads/SignerFinder';
+import { useWallet, WalletMultiButton } from 'solana-wallets-vue';
+import WrapperImageAsync from 'components/wrapper/WrapperImageAsync.vue';
+import WrapperSolscanLink from 'components/wrapper/WrapperSolscanLink.vue';
 
 const rows = [];
 
@@ -41,7 +42,7 @@ watch(
   },
 );
 
-format_address(getSigner().toString());
+format_address('');
 
 const rowsPerPageOptions = computed(() => {
   return $q.screen.gt.xs ? ($q.screen.gt.sm ? [3, 6, 9] : [3, 6]) : [3];
@@ -67,18 +68,9 @@ const rowsPerPageOptions = computed(() => {
         <div class="q-pa-xs col-xs-12 col-sm-6 col-md-3">
           <q-card flat bordered>
             <q-card-section class="text-center">
-              <q-img
-                height="150px"
-                width="150px"
-                :src="
-                  useTokenListStore().tokenList.find(
-                    (t) =>
-                      t.address == props.row.account?.mintUnwrapped.toString(),
-                  )?.logoURI ?? UNKNOWN_IMAGE
-                "
-              />
+              <WrapperImageAsync :wrapper="props.row" />
             </q-card-section>
-            <q-card-section>
+            <q-card-section class="q-gutter-y-xs">
               <div class="row items-center">
                 <div class="col text-subtitle1 text-weight-thin">Group</div>
                 <div class="text-subtitle2">
@@ -130,6 +122,31 @@ const rowsPerPageOptions = computed(() => {
                 <div class="col text-subtitle1 text-weight-thin">Address</div>
                 <div class="text-subtitle2">
                   {{ format_address(props.row?.publicKey?.toString()) }}
+                  <WrapperSolscanLink :account="props.row.publicKey" />
+                </div>
+              </div>
+
+              <div class="row items-center">
+                <div class="col text-subtitle1 text-weight-thin">Unwrapped</div>
+                <div class="text-subtitle2">
+                  {{
+                    format_address(props.row?.account.mintUnwrapped?.toString())
+                  }}
+                  <WrapperSolscanLink
+                    :account="props.row.account.mintUnwrapped"
+                  />
+                </div>
+              </div>
+
+              <div class="row items-center">
+                <div class="col text-subtitle1 text-weight-thin">Wrapped</div>
+                <div class="text-subtitle2">
+                  {{
+                    format_address(props.row?.account.mintWrapped?.toString())
+                  }}
+                  <WrapperSolscanLink
+                    :account="props.row.account.mintWrapped"
+                  />
                 </div>
               </div>
             </q-card-section>
@@ -164,7 +181,14 @@ const rowsPerPageOptions = computed(() => {
               </q-card>
             </q-card-actions>
 
-            <q-card-section class="col">
+            <q-card-section
+              v-if="!useWallet().publicKey.value"
+              class="row justify-center"
+            >
+              <WalletMultiButton dark />
+            </q-card-section>
+
+            <q-card-section v-else class="col">
               <q-card bordered flat class="q-mb-md">
                 <q-card-section>
                   <div class="col text-center">Wallet Balance</div>
