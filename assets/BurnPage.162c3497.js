e00bc8986c694789ab0edeaaa3b22e23210dcbaa
skullnbonesdao <skullnbonesdao@users.noreply.github.com>
2024-12-02 17:33:32 +0000

    deploy: 670d48e0fca85243f07b78ada362d0b1afac29dd

--- assets/BurnPage.162c3497.js
+++
@@ -1,1 +0,0 @@
-import{Q as e}from"./QPage.8bf525fa.js";import{_ as r}from"./plugin-vue_export-helper.21dcd24c.js";import{b1 as a,ai as t,bx as o,ar as s}from"./index.142fab28.js";const c={};function n(f,_){return a(),t(e,{class:"row items-center justify-evenly"},{default:o(()=>[s(" BURN")]),_:1})}var p=r(c,[["render",n]]);export{p as default};
---
+++ assets/rpc.89c0d881.js
@@ -0,0 +1,1 @@
+/* empty css                       */import{b as o}from"./index.fea96a80.js";var a=o(({app:p})=>{console.log("[booted] RPC")});export{a as default};
