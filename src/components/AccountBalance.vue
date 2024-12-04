e81ef4c368ac0002ae39dbbddafe42bf3852f2e5
skullandbonesdao <skullandbonesdao@gmail.com>
2024-12-04 15:49:25 +0100

    added icons to AccountBalance.vue balance element

--- src/components/AccountBalance.vue
+++ src/components/AccountBalance.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
 import { useAccountStore } from 'src/solana/accounts/AccountStore';
 import FormatNumber from 'components/text/FormatNumber.vue';
+import { computed } from 'vue';
 
 const props = defineProps(['mint']);
+
+const icon = computed(() =>
+  useAccountStore().getAccountByMintPublicKey(props.mint)
+    ? useAccountStore().getAccountByMintPublicKey(props.mint)?.info.logoURI
+    : 'unknown.svg',
+);
 </script>
 
 <template>
-  <div class="row">
+  <div class="row items-center q-gutter-x-xs">
     <div class="col text-caption text-weight-light">Wallet-Balance:</div>
+
     <FormatNumber
       :class="
         useAccountStore().getAccountByMintPublicKey(mint)?.uiAmount == 0
@@ -18,6 +26,8 @@ const props = defineProps(['mint']);
       :decimals="4"
       :pad-start="10"
     />
+
+    <q-avatar size="xs"><img :src="icon" /> </q-avatar>
   </div>
 </template>
 
