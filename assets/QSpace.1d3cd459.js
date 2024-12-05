423ef26a1c92d745f2638f99c0772ee8c4b17a84
skullnbonesdao <skullnbonesdao@users.noreply.github.com>
2024-12-05 09:47:01 +0000

    deploy: d88b42e02aeeeddb2be7eef869dc5eb9b9dba4d2

--- assets/QSpace.1d3cd459.js
+++
@@ -1,1 +0,0 @@
-import{aF as a,bZ as e}from"./index.e8af01b4.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
---
+++ assets/rpc.14489ede.js
@@ -0,0 +1,1 @@
+/* empty css                       */import{b as o}from"./index.e3ff99be.js";var a=o(({app:p})=>{console.log("[booted] RPC")});export{a as default};
