6b25da032a7ed004865fa97c968fdb16adf1678c
skullnbonesdao <skullnbonesdao@users.noreply.github.com>
2024-12-14 14:39:41 +0000

    deploy: 8a0d4937dec3236338af98792ea9d121f2fdaeea

--- assets/QCardSection.bcdc60f4.js
+++
@@ -1,1 +0,0 @@
-import{bZ as e,ah as r,aF as n,b_ as s}from"./index.3efe1a99.js";var i=e({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(a,{slots:t}){const o=r(()=>`q-card__section q-card__section--${a.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>n(a.tag,{class:o.value},s(t.default))}});export{i as Q};
